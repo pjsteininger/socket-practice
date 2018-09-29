236160d45fd67c90382492b3cad53f8ced4d6580
Philip S(D) <philipsteininger@gmail.com>
2018-09-28 22:36:52 -0700

    sockets

--- socket-app.js
+++ socket-app.js
@@ -1,15 +1,30 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(req, res){
+server.listen(9999, function () {
+  console.log("server listening on 9999");
+});
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function (socket) {
+  console.log("a user connected: " + socket.id);
+  socket.on("disconnect", function () {
+    console.log("a user disconnected: " + socket.id);
+  })
+  socket.on('chat message', function (msg) {
+    console.log(socket.id + ' message: ' + msg);
+  });
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+io.on("disconnection", function () {
+  console.log("???");
+})
